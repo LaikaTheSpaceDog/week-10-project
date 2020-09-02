33b5e84ce4e9a20b685ada9db7ab3ff62f907d7a
Oscar Wales <oscarjwales@gmail.com>
2020-09-02 14:31:59 +0100

    Completed normal and tricksy AJAX/API challenges

--- src/ajax.js
+++ src/ajax.js
@@ -1,1 +1,43 @@
 import axios from "./axiosconfig";
+
+// axios.get("/articles").then(({data}) => {
+//     console.log(data.data);
+// })
+
+// axios.post("/articles", {title: "blah", tags: ["One", "two"], content: "random text"}).then(({data}) => {
+//     console.log(data.data.id);
+// })
+
+// axios.get("/articles/74").then(({data}) => {
+//     console.log(data.data);
+// })
+
+// axios.put("/articles/74", {title: "blah", tags: ["One", "two", "three"], content: "random text"}).then(({data}) => {
+//     console.log(data.data.tags);
+// })
+
+// axios.delete("/articles/17").then(({data}) => {
+//     console.log("Deleted");
+// })
+
+// axios.post("/articles/16/comments", {email: "test@gmail.com", comment: "La la la"}).then(({data}) => {
+//     console.log(data.data.id);
+// })
+
+// axios.get("/articles/16/comments").then(({data}) => {
+//     console.log(data.data);
+// })
+
+// axios.get("/tags").then(({data}) => {
+//     console.log(data.data);
+// })
+
+// axios.get("/articles/80").then(({data}) => {
+//     console.log(data.data);
+// }, error => {
+//     console.log("Not found");
+// })
+
+axios.get("/articles").then(({data}) => {
+    console.log(data.data.map(article => article.title));
+})
